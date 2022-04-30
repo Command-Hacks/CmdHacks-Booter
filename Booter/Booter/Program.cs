c74c843522545ec060fe061e5104fbb24b735c31
Command-Hacks <104701115+Command-Hacks@users.noreply.github.com>
2022-05-01 01:58:10 +0200

    Add files via upload
    Launch gewoon de .exe .

---
+++ Booter/Booter/Program.cs
@@ -0,0 +1,24 @@
+﻿// Course - 01 - Writelines...
+{
+Console.WriteLine("  ");
+Console.WriteLine("░█████╗░░█████╗░███╗░░░███╗███╗░░░███╗░█████╗░███╗░░██╗██████╗░");
+Console.WriteLine("██╔══██╗██╔══██╗████╗░████║████╗░████║██╔══██╗████╗░██║██╔══██╗");
+Console.WriteLine("██║░░╚═╝██║░░██║██╔████╔██║██╔████╔██║███████║██╔██╗██║██║░░██║");
+Console.WriteLine("██║░░██╗██║░░██║██║╚██╔╝██║██║╚██╔╝██║██╔══██║██║╚████║██║░░██║");
+Console.WriteLine("╚█████╔╝╚█████╔╝██║░╚═╝░██║██║░╚═╝░██║██║░░██║██║░╚███║██████╔╝");
+Console.WriteLine("░╚════╝░░╚════╝░╚═╝░░░░░╚═╝╚═╝░░░░░╚═╝╚═╝░░╚═╝╚═╝░░╚══╝╚═════╝░");
+Console.WriteLine("Kies een hack command!");
+Console.WriteLine("De huidige keuzes zijn : wifiboot , wifiverneukerrr");
+Console.WriteLine("Voer een hack command uit!>$");
+//Preventing Instant-Close
+Console.ReadLine();
+}
+    {
+        Console.WriteLine("Wifi is succesvol geboot voor  500sec");
+        Console.WriteLine("                                 ");
+                Console.WriteLine("Command.Hacks - Signed Official Terms.");
+        Console.WriteLine("Booten van politie domein kan soms niet lukken en is ter strengste verboden + strafbaar!");
+        Console.ReadKey();
+
+    }
+
